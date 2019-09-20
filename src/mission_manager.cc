de81d755169f1bc9e3b7d44970a6a88b07f896be
Mussol MAV <mussol@uib.es>
2019-09-20 17:11:58 +0200

    mission_manager: correcting bug

--- src/mission_manager.cc
+++ src/mission_manager.cc
@@ -36,10 +36,18 @@ void MissionManager::dynReconfig(srv_mav_behaviours::mission_managerConfig &conf
 
   min_height = config.min_height;
   max_height = config.max_height;
-  sweep_y_size = sweep_y_size;
-  sweep_z_size = sweep_z_size;
-  sweep_z_increment = sweep_z_increment;
-  sweep_WP_error = sweep_WP_error;
+
+  if(!performing_sweep){
+
+    sweep_y_size = config.sweep_y_size;
+    sweep_z_size = config.sweep_z_size;
+    sweep_z_increment = config.sweep_z_increment;
+    sweep_WP_error = config.sweep_WP_error;
+
+  }else{
+    ROS_WARN("Sweeping parameters can not be modified now");
+  }
+
 
   checkParameters();
 
@@ -57,23 +65,17 @@ void MissionManager::configure(){
   nh_.param("max_height", max_height, 2.0);
   ROS_INFO("Max height: %2.2f", max_height); // maximum height for the autonomous behaviours
 
-  if(!performing_sweep){
+  nh_.param("sweep_y_size", sweep_y_size, 3.0);
+  ROS_INFO("Sweep_y_size: %2.2f", sweep_y_size);
 
-    nh_.param("sweep_y_size", sweep_y_size, 3.0);
-    ROS_INFO("Sweep_y_size: %2.2f", sweep_y_size);
+  nh_.param("sweep_z_size", sweep_z_size, 3.0);
+  ROS_INFO("Sweep_z_size: %2.2f", sweep_z_size);
 
-    nh_.param("sweep_z_size", sweep_z_size, 3.0);
-    ROS_INFO("Sweep_z_size: %2.2f", sweep_z_size);
+  nh_.param("sweep_z_increment", sweep_z_increment, 1.0);
+  ROS_INFO("Sweep_z_increment: %2.2f", sweep_z_increment);
 
-    nh_.param("sweep_z_increment", sweep_z_increment, 1.0);
-    ROS_INFO("Sweep_z_increment: %2.2f", sweep_z_increment);
-
-    nh_.param("sweep_WP_error", sweep_WP_error, 0.3);
-    ROS_INFO("Sweep_WP_error: %2.2f", sweep_WP_error);
-
-  }else{
-    ROS_WARN("Sweeping parameters can not be modified now");
-  }
+  nh_.param("sweep_WP_error", sweep_WP_error, 0.3);
+  ROS_INFO("Sweep_WP_error: %2.2f", sweep_WP_error);
 
   checkParameters();
 
